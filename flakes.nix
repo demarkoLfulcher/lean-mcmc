fc7808b2e83a653063eb4317e390e71bb0339c91
demarkoLfulcher <demarko.fulchersb@gmail.com>
2026-07-06 12:39:30 -0400

    Added Flakes for dev env

---
+++ flakes.nix
@@ -0,0 +1,23 @@
+{
+  description = "lean-monte-carlo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    lean4.url = "github:leanprover/lean4";
+  };
+
+  outputs = { self, nixpkgs, lean4 }:
+    let
+      system = "x86_64-linux"; 
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "lean-dev-shell";
+
+        buildInputs = with pkgs; [
+          elan 
+          git
+        ];
+      };
+    };
+}
